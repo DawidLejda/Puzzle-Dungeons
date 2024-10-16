d0ecde54fc3091a20b7b0ae3821c39031f8fe8c0
Dawid Lejda <lejdadawid@gmail.com>
2024-10-16 16:51:02 +0200

    implementing key movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,17 +8,25 @@ public class GamePanel extends JPanel implements Runnable
     final int width = 640;
     final int height = 480;
     final int tileSize = 32;
-    final int maxScreenCol = width / tileSize; // 640 pixels width / 32 tileSize = 20
-    final int maxScreenRow = height / tileSize; // 480 pixels height / 32 tileSize = 15
 
     Thread GameThread;
+    KeyHandler pressedKey = new KeyHandler();
+
+
+    public int playerSpeed = 4;
+    public int playerX = 100;
+    public int playerY = 100;
 
     public GamePanel()
     {
         this.setPreferredSize(new Dimension(width,height));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(pressedKey);
+        this.setFocusable(true);
+
     }
+
 
     public void startGameThread()
     {
@@ -29,6 +37,40 @@ public class GamePanel extends JPanel implements Runnable
     @Override
     public void run()
     {
-
+        while (GameThread != null)
+        {
+            playerMovement();
+            repaint();
+        }
     }
+
+    public void playerMovement()
+    {
+        if (pressedKey.left)
+        {
+            playerX -= playerSpeed;
+        }
+        else if (pressedKey.right)
+        {
+            playerX += playerSpeed;
+        }
+        else if (pressedKey.up)
+        {
+            playerY -= playerSpeed;
+        }
+        else if (pressedKey.down)
+        {
+            playerY += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(100,100, tileSize, tileSize);
+        g2.dispose();
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,27 @@
 package main;
 
+import javax.swing.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        new GameFrame();
+        JFrame GameFrame = new JFrame();
+        GameFrame.setTitle("Puzzle Dungeons");
+        GameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GameFrame.setResizable(false);
 
+        // TO DO
+        //ImageIcon image = new ImageIcon();
+        //this.setIconImage(image.getImage());
 
+        GamePanel game_panel = new GamePanel();
+        GameFrame.add(game_panel);
+        GameFrame.pack();
+
+        GameFrame.setLocationRelativeTo(null);
+        GameFrame.setVisible(true);
+
+        game_panel.startGameThread();
     }
 }
