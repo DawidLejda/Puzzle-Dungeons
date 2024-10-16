7327506223b56f099c9c0bacc8d29d87463e783e
Dawid Lejda <lejdadawid@gmail.com>
2024-10-16 21:35:55 +0200

    implementing working game loop, FPS display

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,22 +5,25 @@ import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable
 {
+    // Parameters ************************************************
     final int width = 640;
     final int height = 480;
     final int tileSize = 32;
 
+    final int TARGET_FPS = 60;
+    public int averageFPS;
+
     Thread GameThread;
     KeyHandler pressedKey = new KeyHandler();
-
 
     public int playerSpeed = 4;
     public int playerX = 100;
     public int playerY = 100;
 
+    // ********************************************************
     public GamePanel()
     {
         this.setPreferredSize(new Dimension(width,height));
-        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
         this.addKeyListener(pressedKey);
         this.setFocusable(true);
@@ -37,14 +40,39 @@ public class GamePanel extends JPanel implements Runnable
     @Override
     public void run()
     {
+
+        final double targetTime = 1000000000/TARGET_FPS;
+        double delta = 0;
+        int frameCount = 0;
+        long timer = System.currentTimeMillis();
+        long initialTime = System.nanoTime();
+
+        // Game loop
         while (GameThread != null)
         {
-            playerMovement();
-            repaint();
+            long currentTime = System.nanoTime();
+
+            delta += (currentTime - initialTime) / targetTime;
+            initialTime = currentTime;
+
+            if (delta >= 1)
+            {
+                playerUpdate();
+                repaint();
+                delta--;
+                frameCount++;
+            }
+
+            if (System.currentTimeMillis() - timer > 1000)
+            {
+                averageFPS = frameCount;
+                timer += 1000;
+                frameCount = 0;
+            }
         }
     }
 
-    public void playerMovement()
+    public void playerUpdate()
     {
         if (pressedKey.left)
         {
@@ -68,8 +96,9 @@ public class GamePanel extends JPanel implements Runnable
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.white);
-        g2.fillRect(100,100, tileSize, tileSize);
+        g2.setColor(Color.blue);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.drawString("FPS: " + averageFPS,5,10);
         g2.dispose();
     }
 
